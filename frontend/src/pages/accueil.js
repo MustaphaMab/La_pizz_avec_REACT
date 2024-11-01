7bfe268865d8a625da6e1160db7502c88df41e6e
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-11-02 00:54:43 +0100

    modifs page accueil

--- frontend/src/pages/accueil.js
+++ frontend/src/pages/accueil.js
@@ -7,11 +7,11 @@ function Accueil() {
       <section className="banner" style={{ backgroundImage: "url('/images/banniere.jpg')" }}>
         <div className="banner-overlay">
           <h1 className="main-title">LA PIZZ'</h1>
+          <h2>04 43 59 65 98</h2>
         </div>
       </section>
 
       <section className="about">
-        <p><strong>QUALITE ET AUTHENTICITE</strong></p>
         <p><strong>LIVRAISON GRATUITE</strong></p>
         <p><strong>CUITES AU FEU DE BOIS</strong></p>
       </section>

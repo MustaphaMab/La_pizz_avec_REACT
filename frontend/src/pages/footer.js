23907a789b2fc71f48b5256c7656f92c8b8ed156
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-11-02 14:54:08 +0100

    modifs page accueil

--- frontend/src/pages/footer.js
+++ frontend/src/pages/footer.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa"; // Import des icônes
+import './footer.css';
 
 function Footer() {
     return (
-        <footer>
-            <p>&copy; 2024 La Pizz  - Tous droits réservés.</p>
+        <footer className="footer">
+            <p>&copy; 2024 La Pizz' - Tous droits réservés.</p>
+            <div className="social-icons">
+                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                    <FaFacebookF />
+                </a>
+                <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                    <FaTwitter />
+                </a>
+                <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+                    <FaInstagram />
+                </a>
+            </div>
         </footer>
     );
 }

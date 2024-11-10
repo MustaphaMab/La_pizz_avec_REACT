05429511fc5098ddad7f4ff2912675f1ff1fec1d
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-11-10 10:09:45 +0100

    modifs accueil

--- frontend/src/pages/accueil.js
+++ frontend/src/pages/accueil.js
@@ -9,6 +9,7 @@ function Accueil() {
       <section className="banner" style={{ backgroundImage: "url('/images/banniere.jpg')" }}>
         <div className="banner-overlay">
           <h1 className="main-title">LA PIZZ'A MOOS</h1>
+          <h2>04 43 59 65 98</h2>
         </div>
       </section>
 

95e0e8bcad31cffdd84f6435828d18827402e36b
MustaphaMab <145995419+MustaphaMab@users.noreply.github.com>
2024-11-13 21:43:47 +0100

    test style page contact

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -1,43 +1,54 @@
 import React from "react";
-import { FaPhoneAlt, FaMapMarkerAlt, FaEnvelope, FaClock } from "react-icons/fa";
-import './contact.css';
-
+import {
+  FaPhoneAlt,
+  FaMapMarkerAlt,
+  FaEnvelope,
+  FaClock,
+} from "react-icons/fa";
+import "./contact.css";
 
 // Composant de la page de contact
 function Contact() {
   return (
     <div className="contact-container">
       {/* Titre de la section Contact */}
+
       <h1>Contactez-nous</h1>
-      
-      {/* Description de la section */}
-      <p className="contact-description">
-        Pour toute demande, vous pouvez nous contacter directement via les coordonnées ci-dessous.
-      </p>
+      <div className="container-contact">
+        {/* Description de la section */}
+        <p className="contact-description">
+          Pour toute demande, vous pouvez nous contacter directement via les
+          coordonnées ci-dessous.
+        </p>
 
-      <div className="contact-info">
-        {/* Informations de contact : téléphone */}
-        <div className="contact-item">
-          <FaPhoneAlt className="contact-icon" />
-          <span><a href="tel:+33123456789">Téléphone : 01 23 45 67 89</a></span>
-        </div>
-        
-        {/* Informations de contact : adresse */}
-        <div className="contact-item">
-          <FaMapMarkerAlt className="contact-icon" />
-          <span>Adresse : 3 Bd Michelet, 13008, Marseille, France</span>
-        </div>
+        <div className="contact-info">
+          {/* Informations de contact : téléphone */}
+          <div className="contact-item">
+            <FaPhoneAlt className="contact-icon" />
+            <span>
+              <a href="tel:+33123456789">Téléphone : 01 23 45 67 89</a>
+            </span>
+          </div>
 
-        {/* Informations de contact : email */}
-        <div className="contact-item">
-          <FaEnvelope className="contact-icon" />
-          <span><a href="mailto:contact@lapizz.com">Email : contact@lapizz.com</a></span>
-        </div>
+          {/* Informations de contact : adresse */}
+          <div className="contact-item">
+            <FaMapMarkerAlt className="contact-icon" />
+            <span>Adresse : 3 Bd Michelet, 13008, Marseille, France</span>
+          </div>
 
-        {/* Informations de contact : horaires */}
-        <div className="contact-item">
-          <FaClock className="contact-icon" />
-          <span>Horaires : Lundi - Samedi : 11h - 23h</span>
+          {/* Informations de contact : email */}
+          <div className="contact-item">
+            <FaEnvelope className="contact-icon" />
+            <span>
+              <a href="mailto:contact@lapizz.com">Email : contact@lapizz.com</a>
+            </span>
+          </div>
+
+          {/* Informations de contact : horaires */}
+          <div className="contact-item">
+            <FaClock className="contact-icon" />
+            <span>Horaires : Lundi - Samedi : 11h - 23h</span>
+          </div>
         </div>
       </div>
 
